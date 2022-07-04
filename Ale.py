6c9089d4de39f22e4e78a224949f659c1c4a7b5d
Gerardo Aguayo <geraaguayoc@gmail.com>
2022-07-04 18:36:45 -0500

    Funcion de reconocimiento de voz

--- Ale.py
+++ Ale.py
@@ -1,5 +1,8 @@
+from cgitb import text
 import pyttsx3
 import speech_recognition as sr
+
+r = sr.Recognizer()
 
 #Configuracion de la voz de Ale
 voz = pyttsx3.init()
@@ -9,7 +12,26 @@ def decir(texto):
     voz.say(texto)
     voz.runAndWait()
 
+def escuchar():
+    try:
+        with sr.Microphone() as source:
+            print('Escuchando...')
+            decir('Escuchando')
+            audio = r.listen(source)
+            texto = r.recognize_google(audio)
+            print(texto) 
+            texto.lower()
+            
+    except:
+        print('Intentalo de nuevo')
+        decir('Intentalo de nuevo')
+        pass
+    
+    return texto
 
-decir('Hola Gerardo, me da gusto verte de nuevo')
 
-print('test finalizado !')
+
+
+
+
+
